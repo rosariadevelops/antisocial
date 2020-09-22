94dd15a38f00367db59d9d210c80dc12c6cd86b9
Rosaria Gandar <rosariagandar@gmail.com>
2020-09-22 20:32:45 +0200

    Added error handling because I forgot to

--- src/Registration.js
+++ src/Registration.js
@@ -5,17 +5,23 @@ export default class Registration extends React.Component {
     constructor() {
         super();
         this.state = {
+            first: null,
+            last: null,
+            email: null,
+            password: null,
             error: false,
         };
     }
 
     handleChange(e) {
-        const target = e.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-            [name]: value,
-        });
+        const { name, value } = e.target;
+
+        this.setState(
+            {
+                [name]: value,
+            },
+            () => console.log("this.state: ", this.state)
+        );
 
         // if error is false, render message
     }
@@ -32,23 +38,45 @@ export default class Registration extends React.Component {
         const target = e.target;
         const value = target.value;
         const name = target.name;
-        this.setState(
-            {
-                [name]: value,
-            },
-            () => console.log("this.state: ", this.state)
-        );
-        axios
-            .post("/welcome", this.state)
-            .then(function (response) {
-                console.log("register response:", response);
-                location.replace("/");
-            })
-            .catch(function (err) {
-                console.log("err in  POST /register: ", err);
-            });
+        /* const first = document.querySelector("#first");
+        const last = document.querySelector("#last");
+        const email = document.querySelector("#email");
+        const password = document.querySelector("#password"); */
+        console.log("this.state.error:", this.state.error);
 
-        // if error is false, render message
+        const { first, last, email, password } = this.state;
+
+        if (
+            first === null ||
+            last === null ||
+            email === null ||
+            password === null
+        ) {
+            this.setState(
+                {
+                    error: true,
+                },
+                () => console.log("this.state: ", this.state)
+            );
+        } else {
+            this.setState(
+                {
+                    [name]: value,
+                    error: false,
+                },
+                () => console.log("this.state: ", this.state)
+            );
+
+            axios
+                .post("/welcome", this.state)
+                .then(function (response) {
+                    console.log("register response:", response);
+                    location.replace("/");
+                })
+                .catch(function (err) {
+                    console.log("err in  POST /register: ", err);
+                });
+        }
     }
 
     render() {
@@ -56,7 +84,9 @@ export default class Registration extends React.Component {
             <div className="rgtr-form">
                 <h3>Register here:</h3>
                 {this.state.error && (
-                    <p className="error">Something went wrong!</p>
+                    <p className="error">
+                        Please make sure all input fields have been filled.
+                    </p>
                 )}
                 <label htmlFor="first">Enter your first name:</label>
                 <input
